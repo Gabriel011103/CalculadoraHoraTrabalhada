893698759133a520c56df19e923a4fcb8a3e5945
Gabriel011103 <167925370+Gabriel011103@users.noreply.github.com>
2024-04-25 22:52:00 -0300

    Parte Back End do software
    Parte do back end do software.

--- CalculadoraHora/CalculadoraHora.java
+++ CalculadoraHora/CalculadoraHora.java
@@ -2,18 +2,12 @@ package CalculadoraHora;
 	import java.util.Scanner;
 
 		public class CalculadoraHora {
+			
 			public static void main(String[] args) {
+
 				Scanner scanner = new Scanner (System.in);
-//primeira palavra se refere ao tipo de variavel que a segunda palavra nomeia.
-// o = recebe o valor do comando adiante.
-// se chama o metodo que foi criado.
-				// o que está em pareteses diz que o sistema recebe.
 					System.out.println("Quantas Horas você trabalha por dia ?");
 						double horaTrabalhada = scanner.nextDouble();
-// de novo é declarado o tipo de variavel a ser nomeada.
-// o sinal de = diz que a variavel recebe o valor do metodo com uma ação.
-// o next doule é uma ação do metodo scanner.		
-//Para o script.
 					System.out.println("Quantos dias você Trabalha no mês ?");
 						double diasTrabalhados = scanner.nextDouble();
 					System.out.println("Qual é o valor do seu sálario ?");
@@ -22,5 +16,8 @@ package CalculadoraHora;
 					String valorHoraTrabalhadaFormatada = String.format("%.2f", valorHoraTrabalhada);
 						System.out.println ("A sua hora de trabalho vale R$:" + (valorHoraTrabalhadaFormatada) + ".");
 				scanner.close();
+		
 			}
+
 	}
+
